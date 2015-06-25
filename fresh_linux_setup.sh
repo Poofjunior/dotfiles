40fac97a47f022c1128b2cde591975fb42ce1715
Joshua Vasquez <seeifyoucanmeetme@gmail.com>
2015-06-25 10:33:26 -0700

    created fresh_linux_setup.sh

---
+++ fresh_linux_setup.sh
@@ -0,0 +1,11 @@
+# install terminator
+add-apt-repository ppa:gnome-terminator
+apt-get update
+apt-get install terminator
+
+# set terminator as default terminal
+update-alternatives --config x-terminal-emulator
+
+#install vim
+apt-get install vim
+
