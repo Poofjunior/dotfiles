6c41117195095351e4f7d3452280bdab29baebcc
Joshua Vasquez <seeifyoucanmeetme@gmail.com>
2015-12-03 11:57:29 -0800

    added note about git completion.

--- fresh_linux_setup.sh
+++ fresh_linux_setup.sh
@@ -44,6 +44,8 @@ apt-get install ksnapshot
 
 #install git
 apt-get install git
+#install git completion from here:
+# https://git-scm.com/book/en/v1/Git-Basics-Tips-and-Tricks
 
 # install ros (indigo)
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -63,4 +65,3 @@ wget https://www.pjrc.com/teensy/td_125/teensyduino.64bit
 
 # if sound doesn't work, install pavucontrol
 apt-get install pavucontrol
-
